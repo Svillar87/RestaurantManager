56193b95d923b068c302cfe5c36bf71e270a9c22
Marcos Gomez Medina <margo.med.03@gmail.com>
2022-04-05 20:43:50 +0200

    Marcos

--- app/src/main/java/com/example/restaurantmanager/MainActivity.java
+++ app/src/main/java/com/example/restaurantmanager/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.restaurantmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button bt_incio,bt_registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt_incio= findViewById(R.id.bt_inicio);
+        bt_registro=findViewById(R.id.bt_registro);
+        bt_incio.setOnClickListener(this);
+        bt_registro.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case(R.id.bt_registro):
+                Intent registro = new Intent(this,Registro.class);
+                startActivity(registro);
+                break;
+            case (R.id.bt_inicio):
+                Intent inicio = new Intent(this,Menu.class);
+                startActivity(inicio);
+                break;
+        }
+
+    }
+
 }
